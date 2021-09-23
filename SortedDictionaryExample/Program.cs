a14fa62a76326aa24764c5e23e0458bb6e59bec3
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-23 13:11:21 +0600

    Elements are added.....

--- SortedDictionaryExample/Program.cs
+++ SortedDictionaryExample/Program.cs
@@ -9,6 +9,12 @@ namespace SortedDictionaryExample
         {
             //create a sorted dictionary
             SortedDictionary<int, string> names = new SortedDictionary<int, string>();
+
+            //add elements
+            names.Add(2, "Mishu");
+            names.Add(4, "Rayhan");
+            names.Add(3, "Mithila");
+            names.Add(1, "Shihab");
         }
     }
 }
